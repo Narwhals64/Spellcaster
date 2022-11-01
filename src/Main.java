30d4a063f0052dc315cb1c2a4f954a7135bd0a24
ethanraomiami <raoes@miamioh.edu>
2022-11-01 09:19:17 -0400

    binary search

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     private static Board board;
+    private static String[] dic;
 
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<String> dic = new ArrayList<>();
+        ArrayList<String> dicList = new ArrayList<>();
 
         File dicFile = new File("src/dictionary.txt");
         Scanner reader = new Scanner(dicFile);
         while (reader.hasNextLine()) {
             String word = reader.nextLine();
-            dic.add(word);
+            dicList.add(word);
         }
+
+        dic = dicList.toArray(new String[0]);
 
         String l25 = "ABCDEFGHIJKLMNOPQRSTUVWXY";
 
-        makeBoard(l25);
+        //makeBoard(l25);
 
-        board.print();
+        //board.print();
+
+        String[] newDic = subDic("BANAX");
+        for (String s : newDic) {
+            System.out.println(s);
+        }
 
     }
+
 
     public static void makeBoard(String letStr) {
         board = new Board();
@@ -35,6 +45,73 @@ public class Main {
         }
     }
 
+
+    public static int firstOcc(String word) {
+        int low = 0;
+        int high = dic.length - 1;
+        int index = -1;
+
+        while (low <= high) {
+            index = low  + ((high - low) / 2);
+
+            if (dic[index].startsWith(word) && index == 0) {
+                return index;
+            } else if (dic[index].startsWith(word) && !dic[index-1].startsWith(word)) {
+                return index;
+            } else if (dic[index].startsWith(word)) {
+                high = index - 1;
+            } else if (!dic[index].startsWith(word) && dic[index].compareTo(word) < 0) {
+                low = index + 1;
+            }  else if (!dic[index].startsWith(word) && dic[index].compareTo(word) > 0) {
+                high = index - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static int lastOcc(String word) {
+        int low = 0;
+        int high = dic.length - 1;
+        int index = -1;
+
+        while (low <= high) {
+            index = low  + ((high - low) / 2);
+
+            if (dic[index].startsWith(word) && index == dic.length-1) {
+                return index;
+            } else if (dic[index].startsWith(word) && !dic[index+1].startsWith(word)) {
+                return index;
+            } else if (dic[index].startsWith(word)) {
+                low = index + 1;
+            } else if (!dic[index].startsWith(word) && dic[index].compareTo(word) < 0) {
+                low = index + 1;
+            }  else if (!dic[index].startsWith(word) && dic[index].compareTo(word) > 0) {
+                high = index - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    /**
+     * Returns a dictionary of words that
+     * start with the given prefix.
+     * @param prefix
+     * @return
+     */
+    public static String[] subDic(String prefix) {
+        int start = firstOcc(prefix);
+        int end = lastOcc(prefix);
+
+        if (start == -1 || end == -1) {
+            return new String[0];
+        }
+
+        return Arrays.copyOfRange(dic, start, end+1);
+    }
 
 
 
